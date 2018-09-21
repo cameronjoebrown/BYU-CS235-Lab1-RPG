28f620665c8b0ee37a5b8c7dba45e0540e660f4a
Cameron Joseph Brown <camquarter2015@gmail.com>
2018-09-21 17:17:30 -0600

    Initial Commit

---
+++ Lab-1-RPG/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab-1-RPG
+//
+//  Created by Cam on 9/21/18.
+//  Copyright © 2018 Koti. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
